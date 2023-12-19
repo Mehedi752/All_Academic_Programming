98f444f842e45ad5a6a9226e26c6057d29de69f4
Mehedi Hasan <130550710+Mehedi752@users.noreply.github.com>
2023-12-19 12:46:58 +0600

    Create Newton_Raphson_Method.cpp

---
+++ Numerical Analysis/Newton_Raphson_Method.cpp
@@ -0,0 +1,43 @@
+// Mehedi Hasan @Mehedi752
+#pragma GCC optimize("Ofast")
+#include <bits/stdc++.h>
+using namespace std;
+
+double error_find(double c_experimental, double c_actual)
+{
+    return abs(c_experimental - c_actual) / c_actual;
+}
+
+double f(double x)
+{
+    return 3 * x - cos(x) - 1;
+}
+
+double df(double x) // First Derivative of f(x).
+{
+    return 3 + sin(x);
+}
+
+double Newton_Raphson(double x, double ea)
+{
+    double error_tm = 1, c_experimental, c;
+    while (error_tm > ea)
+    {
+        c_experimental = x;
+        c = c_experimental - (f(x) / df(x));
+        if (f(c) == 0)
+            break;
+        error_tm = error_find(c_experimental, c);
+        x = c;
+    }
+    return c;
+}
+
+int main()
+{
+    double a, b, ea = 0.0000000000001;
+    cin >> a >> b;
+    double x = a;
+    double root = Newton_Raphson(x, ea);
+    cout << root << endl;
+}
