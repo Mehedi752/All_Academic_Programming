82c5ad256043b5dae6f64b6d4db682d6d274ebd1
Mehedi Hasan <130550710+Mehedi752@users.noreply.github.com>
2024-03-11 11:39:58 +0600

    Create Linear_Search.cpp

---
+++ Data Structure & Algorithm/Searching/Linear_Search.cpp
@@ -0,0 +1,36 @@
+// Mehedi Hasan @Mehedi752
+#pragma GCC optimize("Ofast")
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define MOD 1000000007
+
+void Mehedi()
+{
+    ll n;
+    cin >> n;
+    vector<ll> arr(n);
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    ll x, index = -1;
+    cin >> x;
+    for (int i = 0; i < n; i++)
+        if (arr[i] == x)
+            index = i;
+
+    if (index == -1)
+        cout << "Search Unsuccessful" << '\n';
+    else
+        cout << index + 1 << '\n';
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+    int t = 1;
+    // cin >> t;
+    for (int i = 1; i <= t; i++)
+        Mehedi();
+    return 0;
+}
